de6a0b912e50d9c7c076b995b67a30108ec2d218
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-02-20 21:56:37 -0300

    feat - Criação do quizz, funções para exibir as questões, captar e validar os valores dos inputs

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function displayMessages(anwsers) {
 }
 
 
+
 getData();
 
 
--- scripts/createQuizz.js
+++ scripts/createQuizz.js
@@ -1,12 +1,14 @@
 //const URLTEST = new RegExp("^((http(s?):\/\/(www.)?[a-z]+.com\/)|(magnet:\?xt=urn:btih:))|(http(s?):\/\/[a-z]?[0-9]\/)")
 const URLTEST = new RegExp(/^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/);
+const COLORTEST = new RegExp('^#?([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})|([0-9a-fA-F]{3})$');
 let dataSubScreen1 = {};
+let dataSubScreen2 = [];
 const MINVALUEINPUT = 4;
 const MAXVALUEINPUT = 65;
 
 
 /*=======PEGA DADOS DA SUB-TELA 1======*/
-function handleGetInfos(containerInputs, nextScreen) {
+function handleGetBasicInfos(containerInputs, nextScreen) {
     let contentInputs = document.querySelector(`.${containerInputs}`);
     let inputs = contentInputs.querySelectorAll('input');
     let contInputNoFilled = 0
@@ -23,7 +25,7 @@ function handleGetInfos(containerInputs, nextScreen) {
             quantityLevels: inputs[3].value,
         }
         nextSubScreen();
-        getDataQuestions(nextScreen)
+        displayInputQuestions(nextScreen)
     }
 }
 
@@ -60,6 +62,14 @@ function verifyType(input) {
         if (Number(input.min) > Number(input.value)) {
             return false
         }
+    }else if(input.type === 'tel') {
+        if (COLORTEST.test(`${input.value}`)){
+            return true;
+        }else{
+            return false;
+        }
+    }else{
+        return false;
     }
 }
 
@@ -67,10 +77,15 @@ function verifyType(input) {
 function nextSubScreen() {
     let subScreenOne = document.querySelector('#sub-screen1');
     let subScreenTwo = document.querySelector('#sub-screen2');
+    let subScreenThree = document.querySelector('#sub-screen3');
+    
 
     if (subScreenOne.classList.contains('active')) {
         subScreenOne.classList.remove('active');
         subScreenTwo.classList.add('active');
+    }else if (subScreenTwo.classList.contains('active')) {
+        subScreenTwo.classList.remove('active');
+        subScreenThree.classList.add('active');
     }
 }
 
@@ -83,56 +98,158 @@ function handleEditInfoCard(header) {
     boxInputs.querySelector('.inputs-qt-and-lvls').classList.add('active')
 }
 
-function getDataQuestions(nextScreen) {
+
+/*=======MOSTRA NA TELA AS QUESTOES======*/
+function displayInputQuestions(nextScreen) {
     let screenID = nextScreen;
     let screen = document.querySelector(`#${screenID}`);
-    let container = screen.querySelector('.container-inputs-qts-and-lvls');
 
     for (let i = 0; i < Number(dataSubScreen1.quantityQuestions); i++) {
-        container.innerHTML += `
-            <section class="wrongs-and-correct">
+        screen.innerHTML += `
+            <section class="box-inputs">
                 <!-- para esconder o icone deve adcionar a class="disabled" -->
                 <div class="title-qt-and-lvl">
                     <h2>Pergunta ${i+1}</h2>
-                    <ion-icon onclick="handleEditInfoCard(this)" name="create-outline"></ion-icon>
+                    <ion-icon onclick="handleEditInfoCard(this)" name="create-outline" role="img" class="md hydrated" aria-label="create outline"></ion-icon>
                 </div>
 
                 <!-- para esconder o icone deve adcionar a class="active" -->
                 <section class="inputs-qt-and-lvls">
-                    <div class="question-and-image_url">
-                        <input type="text" placeholder="Texto da pergunta" min="20">
-                        <input type="tel" placeholder="Cor de fundo da pergunta">
-                    </div>
+                    <input type="text" placeholder="Texto da pergunta" min="20">
+                    <input type="tel" placeholder="Cor de fundo da pergunta">
 
                     <h2>Resposta correta</h2>
-                    <div class="question-and-image_url">
-                        <input type="text" placeholder="Resposta correta">
-                        <input type="url" placeholder="URL da imagem">
-                    </div>
+                    <input type="text" placeholder="Resposta correta">
+                    <input type="url" placeholder="URL da imagem">
 
                     <h2>Respostas incorretas</h2>
-                    <div class="question-and-image_url"">
-                        <input type="text" placeholder="Resposta incorreta 1" min="1">
-                        <input type="url" placeholder="URL da imagem 1">
-                        <input type="text" placeholder="Resposta incorreta 2" min="1">
-                        <input type="url" placeholder="URL da imagem 2">
-                        <input type="text" placeholder="Resposta incorreta 3" min="1">
-                        <input type="url" placeholder="URL da imagem 3">
-                    </div>
+                    <input type="text" placeholder="Resposta incorreta 1" min="1" class='wrong'>
+                    <input type="url" placeholder="URL da imagem 1" class='wrong'>
+
+                    <input type="text" placeholder="Resposta incorreta 2" min="1" class='wrong'>
+                    <input type="url" placeholder="URL da imagem 2" class='wrong'>
+
+                    <input type="text" placeholder="Resposta incorreta 3" min="1" class='wrong'>
+                    <input type="url" placeholder="URL da imagem 3" class='wrong'>
                 </section>
             </section>
         `
+
     }
+    screen.innerHTML += `
+        <button class="btn-main" onclick="handleGetQuestionsInfo('sub-screen2')">
+            Prosseguir para criar niveis
+        </button>
+    `
+}
+
+/*=======PEGA DADOS DA TELA DE QUESTOES======*/
+function handleGetQuestionsInfo(subScreen) {
+    let screen = document.querySelector(`#${subScreen}`);
+    let inputs = screen.querySelectorAll('input');
+    let contInputNoFilled = 0;
+    let questions = screen.querySelectorAll('.inputs-qt-and-lvls');
+    
+    inputs.forEach(input => contInputNoFilled += verifyInputQuestionsIsFilled(input));
+
+    if (contInputNoFilled > 0) {
+        window.alert("um ou mais campos não foi preenchido corretamente")
+    }else{
+        for (let i = 0; i < questions.length; i++) {
+            let input = questions[i].querySelectorAll('input');
+
+            let data = {
+                title: input[0].value,
+                color: input[1].value,
+                answers: [
+                    {
+                        text: input[2].value,
+                        image: input[3].value,
+                        isCorrectAnswer: true,
+                    },
+                    {
+                        text: input[4].value,
+                        image: input[5].value,
+                        isCorrectAnswer: false,
+                    },
+                    {
+                        text: input[6].value,
+                        image: input[7].value,
+                        isCorrectAnswer: false,
+                    },
+                    {
+                        text: input[8].value,
+                        image: input[9].value,
+                        isCorrectAnswer: false,
+                    },
+                ]
+            }
+
+            dataSubScreen2.push(data)
+            // console.log(data)
+        }
+        nextSubScreen();
+        displayInputLevels()
+    }
+}
+
+/*=======VERIFICA SE OS INPUTS DAS QUESTOES ESTÃO VAZIOS======*/
+function verifyInputQuestionsIsFilled(input) {
+    let cont = 0;
+
+    if (input.value === "" || verifyType(input) === false) {
+        input.classList.add('is-not-filled')
+        input.value = ""
+        cont++
+    }else {
+        input.classList.remove('is-not-filled')
+        if (cont > 0) {
+            cont--;
+        }
+    }
+    return cont;
 }
 
 function getSubScreen(screenID){
     return screenID;
 }
 
-function getValueQuestions() {
-    let allInputQuestions = [document.querySelectorAll('.create-questions .wrong')];
+// function getValueQuestions() {
+//     let allInputQuestions = [document.querySelectorAll('.create-questions .wrong')];
     
 
-    console.log(allInputQuestions)
-}
+//     console.log(allInputQuestions)
+// }
 
+
+function displayInputLevels() {
+    //let screenID = nextScreen;
+    let screen = document.querySelector('#sub-screen3');
+    console.log(screen)
+
+    for (let i = 0; i < Number(dataSubScreen1.quantityLevels); i++) {
+        screen.innerHTML += `
+            <section class="box-inputs">
+                <!-- para esconder o icone deve adcionar a class="disabled" -->
+                <div class="title-qt-and-lvl">
+                    <h2>Nivel ${i+1}</h2>
+                    <ion-icon onclick="handleEditInfoCard(this)" name="create-outline" role="img" class="md hydrated" aria-label="create outline"></ion-icon>
+                </div>
+
+                <!-- para esconder o icone deve adcionar a class="active" -->
+                <section class="inputs-qt-and-lvls">
+                    <input type="text" placeholder="Título do nível">
+                    <input type="number" name="percentage" id="" placeholder="% de acerto mínima">
+                    <input type="url" name="url-image-level" id="" placeholder="URL da imagem do nível">
+                    <textarea name="description of level" id="" cols="30" rows="10" placeholder="Descrição do nível"></textarea>
+                </section>
+            </section>
+        `
+
+    }
+    screen.innerHTML += `
+        <button class="btn-main" onclick="handleGetQuestionsInfo('sub-screen3')">
+            Prosseguir para criar niveis
+        </button>
+    `
+}
